fc6faca2616a463774ca16a68a526b096997a186
TH <tunxero@hotmail.com>
2024-07-10 16:30:19 +0300

    Changed package.json mainly

--- passport-config.js
+++ passport-config.js
@@ -1,36 +1,39 @@
 
 // This code has been taken from passport-config.js from Erno Vanhalas course material sources.
 
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
 function initialize(passport, getUserByUsername, getUserById) {
     const authenticateUser = async (username, password, done) => {
         const user = getUserByUsername(username);
-        if (user === null) {
+        if (user == null) {
             console.log("User not found");
-            return done(null, false);
+            return done(null, false, { message: "No user with that username" });
         }
 
         try {
             if (await bcrypt.compare(password, user.password)) {
+                console.log("Password match:", user);
                 return done(null, user);
             } else {
-                return done(null, false);
+                console.log("Password incorrect:", user);
+                return done(null, false, { message: "Password incorrect" });
             }
         } catch (err) {
-            console.error("Error: ", err);
             return done(err);
         }
-    }
-
+    };
 
     passport.use(new LocalStrategy(authenticateUser));
-    passport.serializeUser((user, done) => done(null, user.id));
+    passport.serializeUser((user, done) => {
+        console.log("Serial:", user);
+        done(null, user.id);
+    });
     passport.deserializeUser((id, done) => {
+        console.log("Deserial:", id);
         return done(null, getUserById(id));
     });
 }
-
 
 module.exports = initialize;
